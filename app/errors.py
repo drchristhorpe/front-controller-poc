72c7ca62975b2d793fbc0d659f4f0632bea4c129
Chris Thorpe <drchristhorpe@googlemail.com>
2018-07-08 20:25:05 +0100

    Added in requirements and template fragments

--- app/errors.py
+++ app/errors.py
@@ -1,13 +1,13 @@
 from flask import render_template
 
 def not_found(url):
-    return render_template('errors/404.html', url=url), 404
+    return render_template('errors/404.html', url=url, route_details={}), 404
 
 def not_authorised(url):
-    return render_template('errors/401.html', url=url), 401
+    return render_template('errors/401.html', url=url, route_details={}), 401
 
 def forbidden(url):
-    return render_template('errors/403.html', url=url), 403
+    return render_template('errors/403.html', url=url, route_details={}), 403
 
 def server_error(url):
-    return render_template('errors/500.html', url=url), 500
+    return render_template('errors/500.html', url=url, route_details={}), 500
