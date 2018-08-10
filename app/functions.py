968f815709320286a3f55b657bac294f5bdb419c
Chris Thorpe <drchristhorpe@googlemail.com>
2018-08-10 09:00:05 +0100

    Support for JSON and Markdown

--- app/functions.py
+++ app/functions.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, Markup
 import requests
 import json
+import markdown
 
+import logging
 
 def read_route_file(filepath, local):
     f = open(filepath, 'r')
@@ -25,14 +27,30 @@ def load_root(filepath, local=True):
 
 
 def handle_request(template_name, route_details):
-    try:
-        data = fetch_url('https://drchristhorpe.github.io/')
-    except:
-        data = None
+    url = route_details['root']
+    if 'url' in route_details and route_details['strip']:
+        try:
+            url = url + route_details['url'].replace(route_details['strip'],'')
+        except:
+            url = url
+    if 'format' in route_details and route_details['format'] == 'json':
+        status, data = fetch_url(route_details['root'])
+        data = json.loads(data)
+    elif 'format' in route_details and route_details['format'] == 'markdown':
+        status, data = fetch_url(route_details['root'])
+        data = Markup(markdown.markdown(data))
+    else:
+        status, data = fetch_url(url)
 
-    return render_template(template_name + '.html', route_details=route_details, data=data), 200
+    if data:
+        return render_template(template_name + '.html', route_details=route_details, data=data), 200
+    else:
+        return render_template('errors/' + str(status) + '.html', route_details=route_details), status
 
 
 def fetch_url(url, format='html'):
     r = requests.get(url)
-    return r.text
+    if r.status_code == 200:
+        return 200, r.text
+    else:
+        return r.status_code, False
